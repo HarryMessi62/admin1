539379f574c9f2aa9c5205f18d630c942d5cde29
igstX4 <programx41@gmail.com>
2025-07-02 14:13:27 +0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://infocryptox.com',
-        target: 'http://localhost:5000',
+        target: 'https://infocryptox.com',
+        // target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       }
